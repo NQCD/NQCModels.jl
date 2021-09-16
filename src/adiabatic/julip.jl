d85637c77f8fa6e6a34db702d606a6a789e17060
James Gardner <james.gardner1421@gmail.com>
2021-09-16 10:16:53 +0100

    Simplify and fix JuLIP model

--- src/adiabatic/julip.jl
+++ src/adiabatic/julip.jl
@@ -1,67 +1,28 @@
-import .JuLIP
 
-using NonadiabaticDynamicsBase: PeriodicCell, Atoms
-using NonadiabaticDynamicsBase: au_to_u, au_to_ang, eV_to_au, eV_per_ang_to_au
-
+using NonadiabaticDynamicsBase: au_to_ang, eV_to_au, eV_per_ang_to_au
+using .JuLIP: JuLIP
 
 export JuLIPModel
 
 """
-    JuLIPModel(atoms::Atoms{N,T}, cell::PeriodicCell,
-               calculator::JuLIP.AbstractCalculator) where {N,T}
+    struct JuLIPModel{T} <: AdiabaticModel
 
 Model for interfacing with JuLIP potentials.
 """
-mutable struct JuLIPModel{T,A<:NamedTuple,B<:NamedTuple} <: AdiabaticModel
+struct JuLIPModel{T} <: AdiabaticModel
     atoms::JuLIP.Atoms{T}
-    tmp::A
-    tmp_d::B
-    function JuLIPModel(atoms::Atoms{N,T}, cell::PeriodicCell,
-                        calculator::JuLIP.AbstractCalculator) where {N,T}
-
-        jatoms = JuLIP.Atoms{T}(;
-            X=zeros(3,length(atoms)),
-            P=zeros(3,length(atoms)),
-            M=au_to_u.(atoms.masses),
-            Z=JuLIP.AtomicNumber.(atoms.numbers),
-            cell=au_to_ang.(cell.vectors'),
-            pbc=cell.periodicity,
-            calc=calculator)
-
-        tmp = JuLIP.alloc_temp(calculator, jatoms)
-        tmp_d = JuLIP.alloc_temp_d(calculator, jatoms)
-
-        new{T,typeof(tmp),typeof(tmp_d)}(jatoms, tmp, tmp_d)
-    end
 end
 
 NonadiabaticModels.ndofs(::JuLIPModel) = 3
 
 function NonadiabaticModels.potential(model::JuLIPModel, R::AbstractMatrix)
     JuLIP.set_positions!(model.atoms, au_to_ang.(R))
-    try
-        V = JuLIP.energy!(model.tmp, model.atoms.calc, model.atoms)
-        return eV_to_au(V)
-    catch e
-        if e isa BoundsError
-            model.tmp = JuLIP.alloc_temp(model.atoms.calc, model.atoms)
-            V = JuLIP.energy!(model.tmp, model.atoms.calc, model.atoms)
-            return eV_to_au(V)
-        else
-            throw(e)
-        end
-    end
+    return eV_to_au(JuLIP.energy(model.atoms))
 end
 
 function NonadiabaticModels.derivative!(model::JuLIPModel, D::AbstractMatrix, R::AbstractMatrix)
     JuLIP.set_positions!(model.atoms, au_to_ang.(R))
-    try
-        JuLIP.forces!(JuLIP.vecs(D), model.tmp_d, model.atoms.calc, model.atoms)
-    catch e
-        if e isa BoundsError
-            model.tmp_d = JuLIP.alloc_temp_d(model.atoms.calc, model.atoms)
-            JuLIP.forces!(JuLIP.vecs(D), model.tmp_d, model.atoms.calc, model.atoms)
-        end
-    end
-    D .= -eV_per_ang_to_au.(D)
+    force = JuLIP.forces(model.atoms)
+    D .= -eV_per_ang_to_au.(JuLIP.mat(force))
+    return D
 end
