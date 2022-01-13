708d989e43047c31aa3d7a303416aff4f6636e7d
James Gardner <38594562+jamesgardner1421@users.noreply.github.com>
2022-01-13 10:00:58 +0000

    feat: add AveragedPotential (#12)

---
+++ src/adiabatic/averaged_potential.jl
@@ -0,0 +1,27 @@
+
+struct AveragedPotential{M,T} <: AdiabaticModel
+    models::M
+    tmp_derivative::Matrix{T}
+end
+
+AveragedPotential(models, r) = AveragedPotential(models, zero(r))
+
+NQCModels.ndofs(model::AveragedPotential) = NQCModels.ndofs(model.models[1])
+
+function NQCModels.potential(model::AveragedPotential, r::AbstractMatrix)
+    V = zero(eltype(r))
+    for m in model.models
+        V += NQCModels.potential(m, r)
+    end
+    return V / length(model.models)
+end
+
+function NQCModels.derivative!(model::AveragedPotential, D::AbstractMatrix, r::AbstractMatrix)
+    copyto!(D, zero(eltype(D)))
+    for m in model.models
+        NQCModels.derivative!(m, model.tmp_derivative, r)
+        D .+= model.tmp_derivative
+    end
+    D ./= length(model.models)
+    return D
+end
