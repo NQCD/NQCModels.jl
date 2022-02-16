94151a28db557ec665db230f8dc639f399a11075
Connor Box <Connor.Box@warwick.ac.uk>
2022-02-16 17:53:53 +0000

    Now works, thanks James

--- src/adiabatic/emt_tian2_interface.jl
+++ src/adiabatic/emt_tian2_interface.jl
@@ -1,4 +1,4 @@
-
+using Libdl
 """
 AdiabaticASEModel{A} <: AdiabaticModel
 
@@ -18,20 +18,27 @@ Implements both `potential` and `derivative!`.
 """
 
 struct AdiabaticEMTModel{A,F} <: AdiabaticModel
-    atoms::A
-    cell::PeriodicCell
-    wrapper_function::F
-    pes_path::AbstractString
+    atoms#::A
+    cell#::PeriodicCell
+    wrapper_function#::F
+    pes_path#::AbstractString
+    function AdiabaticEMTModel(atoms, cell, lib_path, pes_path)
+        library = Libdl.dlopen(lib_path)
+        #library = Libdl.dlopen("/home/chem/msrvhs/git_repos/md_tian2/src/md_tian2_lib.so")
+        wrapper = Libdl.dlsym(library, :force_mp_full_energy_force_wrapper_)
+        new{typeof(atoms),typeof(wrapper)}(atoms, cell, wrapper, pes_path)
+    end
 end
 
 NQCModels.ndofs(::AdiabaticEMTModel) = 3
 
 
-function AdiabaticEMTModel(atoms, cell, lib_path, pes_path)
-    library = Libdl.dlopen(lib_path)
-    wrapper = Libdl.dlsym(library, :force_mp_full_energy_force_wrapper_)
-    return AdiabaticEMTModel(atoms, cell, wrapper, pes_path)
-end
+# function AdiabaticEMTModel(atoms, cell, lib_path, pes_path)
+#     library = Libdl.dlopen(lib_path)
+#     #library = Libdl.dlopen("/home/chem/msrvhs/git_repos/md_tian2/src/md_tian2_lib.so")
+#     wrapper = Libdl.dlsym(library, :force_mp_full_energy_force_wrapper_)
+#     return AdiabaticEMTModel(atoms, cell, wrapper, pes_path)
+# end
     
 function NQCModels.potential(model::AdiabaticEMTModel, R::AbstractMatrix)
     # set_coordinates!(model, R)
@@ -43,7 +50,7 @@ function NQCModels.potential(model::AdiabaticEMTModel, R::AbstractMatrix)
     cell_array = model.cell.vectors
 
     r = zeros(3,nbeads,natoms) 
-    r[:,1,:] = transpose(R[:,:])
+    r[:,1,:] = R[:,:]
     f = zeros(3,nbeads,natoms) #forces
     V = [Float64(0.)] #size nbeads
 
@@ -57,15 +64,14 @@ function NQCModels.potential(model::AdiabaticEMTModel, R::AbstractMatrix)
     surface_element = Vector{UInt8}(string(model.atoms.types[2]))
     is_proj = [true,false]
 
+
     pes_file = Vector{UInt8}(model.pes_path)
 
     @assert natoms == sum(natoms_list)
     @assert length(is_proj) == ntypes
 
-    # This returns forces and energies, can I both in one function? 
-    #ccall((:force_mp_full_energy_and_force_wrapper_,lib_file),
 
-    ccall(eval(model.wrapper_function),
+    ccall(model.wrapper_function,
             Cvoid,
 
             (Ref{Int32},Ref{Int32},Ref{Int32},Ptr{Float64},Ptr{Float64},
@@ -77,15 +83,57 @@ function NQCModels.potential(model::AdiabaticEMTModel, R::AbstractMatrix)
             natoms_list, projectile_element, surface_element, is_proj)
 
 
-    return austrip(V * u"eV")
+    return austrip(V[1] * u"eV")
 end
 
-# function NQCModels.derivative!(model::AdiabaticEMTModel, D::AbstractMatrix, R::AbstractMatrix)
-# set_coordinates!(model, R)
-# D .= -model.atoms.get_forces()'
-# @. D = austrip(D * u"eV/Å")
-# return D
-# end
+function NQCModels.derivative!(model::AdiabaticEMTModel, R::AbstractMatrix)
+
+    natoms = size(model.atoms.types)[1]
+    nbeads = 1 #Kept for future use?
+    ntypes = 2 #Projectile and lattice
+
+    cell_array = model.cell.vectors
+
+    r = zeros(3,nbeads,natoms) 
+    r[:,1,:] = R[:,:]
+    f = zeros(3,nbeads,natoms) #forces
+    V = [Float64(0.)] #size nbeads
+
+    natoms_list = zeros(Int32,ntypes)
+    natoms_list[1] = 1 #one projectile
+    natoms_list[2] = natoms - 1 # n-1 lattice atoms
+
+    # Assume one projectile that is atom 1
+    # Assume one lattice with same elements
+    projectile_element = Vector{UInt8}(string(model.atoms.types[1])) #code wants this to check pes file is right
+    surface_element = Vector{UInt8}(string(model.atoms.types[2]))
+    is_proj = [true,false]
+
+
+    pes_file = Vector{UInt8}(model.pes_path)
+
+    @assert natoms == sum(natoms_list)
+    @assert length(is_proj) == ntypes
+
+
+    ccall(model.wrapper_function,
+            Cvoid,
+
+            (Ref{Int32},Ref{Int32},Ref{Int32},Ptr{Float64},Ptr{Float64},
+            Ptr{Float64},Ptr{Float64},Ptr{UInt8},Ref{Int},
+            Ptr{Int32},Ptr{UInt8},Ptr{UInt8},Ptr{BlasInt}),
+
+            natoms, nbeads, ntypes, r, cell_array,
+            f, V, pes_file, size(pes_file), 
+            natoms_list, projectile_element, surface_element, is_proj)
+
+
+    D = f[:,1,:]
+    #D .= -model.atoms.get_forces()'
+    @. D = austrip(D * u"eV/Å")
+
+    return D
+end
 
 # function set_coordinates!(model::AdiabaticEMTModel, R)
 #     model.atoms.set_positions(ustrip.(auconvert.(u"Å", R')))
