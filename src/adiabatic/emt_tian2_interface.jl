2d05aec9d443d3f2f9e303be7a9fc29d1f12a50a
Connor Box <Connor.Box@warwick.ac.uk>
2022-02-20 12:41:30 +0000

    add cleanup func

--- src/adiabatic/emt_tian2_interface.jl
+++ src/adiabatic/emt_tian2_interface.jl
@@ -147,6 +147,14 @@ function NQCModels.derivative!(model::AdiabaticEMTModel, D::AbstractMatrix, R::A
     return D
 end
 
+function NQCModels.cleanup(model::AdiabaticEMTModel)
+
+    deallocate= Libdl.dlsym(library, :wrapper_mp_wrapper_deallocations_)
+    ccall(deallocate,Cvoid,())
+    library = Libdl.dlclose(lib_path)
+
+end
+
 # function set_coordinates!(model::AdiabaticEMTModel, R)
 #     model.atoms.set_positions(ustrip.(auconvert.(u"Å", R')))
 # end
