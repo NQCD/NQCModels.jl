fc451f9269fa01cbce0d888d4c8a40e31698bfb3
clbox <connor.box@warwick.ac.uk>
2022-12-14 12:34:16 +0000

    fix emt

--- src/adiabatic/emt_tian2_interface.jl
+++ src/adiabatic/emt_tian2_interface.jl
@@ -26,9 +26,12 @@ struct md_tian2_EMT{F,L} <: AdiabaticModel
     natoms::Int
     nbeads::Int
     V::Vector{Float64}
+    freeze::Vector{Int}
+    mobile_atoms::Vector{Int}
 end
 
-function md_tian2_EMT(atoms, cell, lib_path, pes_path)
+
+function md_tian2_EMT(atoms, cell, lib_path, pes_path; freeze=[])
     library = Libdl.dlopen(lib_path)
     pes_init = Libdl.dlsym(library, :wrapper_mp_wrapper_read_pes_)
     wrapper =  Libdl.dlsym(library, :wrapper_mp_wrapper_energy_force_)
@@ -39,12 +42,17 @@ function md_tian2_EMT(atoms, cell, lib_path, pes_path)
     ntypes = 2 #Projectile and lattice
 
     # Md_tian2 seems to use row_major order (same as printed in POSCAR file)
-    cell_array = permutedims(cell.vectors,(2,1))
+    # cell_array = permutedims(cell.vectors,(2,1))
+    cell_array = cell.vectors
 
+    cell_array = auconvert.(u"Å", cell_array)/u"Å"
 
     natoms_list = zeros(Int32,ntypes)
     natoms_list[1] = 1 #one projectile
     natoms_list[2] = natoms - 1 # n-1 lattice atoms
+
+
+    mobile_atoms = setdiff(1:natoms, freeze)
 
     # Assume one projectile that is atom 1
     # Assume one lattice with same elements
@@ -74,10 +82,12 @@ function md_tian2_EMT(atoms, cell, lib_path, pes_path)
 
     V = zeros(Float64, nbeads)
 
-    md_tian2_EMT(wrapper, library, r, f, natoms, nbeads, V)
+    md_tian2_EMT(wrapper, library, r, f, natoms, nbeads, V, freeze, mobile_atoms)
 end
 
 NQCModels.ndofs(::md_tian2_EMT) = 3
+NQCModels.mobileatoms(model::md_tian2_EMT, ::Int) = model.mobile_atoms
+NQCModels.mobileatoms(model::md_tian2_EMT) = model.mobile_atoms
 
 function NQCModels.potential(model::md_tian2_EMT, R::AbstractMatrix)
     set_coordinates!(model, R)
@@ -101,8 +111,6 @@ function NQCModels.derivative!(model::md_tian2_EMT, D::AbstractMatrix, R::Abstra
     set_coordinates!(model, R)
     fill!(model.f, zero(eltype(model.f)))
 
-   
-
     ccall(model.wrapper_function,
         Cvoid,
 
@@ -113,7 +121,7 @@ function NQCModels.derivative!(model::md_tian2_EMT, D::AbstractMatrix, R::Abstra
         model.r, model.f, model.V
     )
 
-    for i in 1:model.natoms
+    @inbounds for i in NQCModels.mobileatoms(model)
         for j in 1:3
             D[j,i] = -austrip(model.f[j,1,i] * u"eV/Å")
         end
@@ -128,5 +136,27 @@ function set_coordinates!(model::md_tian2_EMT, R)
             model.r[j,1,i] = ustrip(auconvert(u"Å", R[j,i]))
         end
     end
+end
+
+function find_layer_indices(r, layers)
+    freeze = Int[]
+    layers == 0 && return freeze
+
+    z_coordinates = @view r[3,:]
+    permutation = sortperm(z_coordinates)
+    ordered_z = z_coordinates[permutation]
+    current_z = ordered_z[begin]
+    current_layer = 1
+
+    for (i, z) in enumerate(ordered_z)
+        if !isapprox(z, current_z)
+            current_layer += 1
+            current_layer > layers && break
+            current_z = z
+        end
+        push!(freeze, permutation[i])
+    end
+    
+    return freeze
 end
 
