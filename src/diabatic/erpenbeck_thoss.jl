d58e8d3dce13dd060d5e9f665a87fb1eb9f90666
Louhokseson <974766935@qq.com>
2024-01-30 13:06:51 +0000

    added potential comment

--- src/diabatic/erpenbeck_thoss.jl
+++ src/diabatic/erpenbeck_thoss.jl
@@ -58,8 +58,8 @@ end
 
 function NQCModels.potential(model::ErpenbeckThoss, r::Real)
     (;morse, c, D₁, D₂, x₀′, a′, V∞) = model
-    ϵ₀(x) = NQCModels.potential(morse, x) + c #neutral - Morse potential
-    ϵ₁(x) = D₁*exp(-2a′*(x-x₀′)) - D₂*exp(-a′*(x-x₀′)) + V∞ #charged
+    ϵ₀(x) = NQCModels.potential(morse, x) + c #neutral - Morse potential U_0
+    ϵ₁(x) = D₁*exp(-2a′*(x-x₀′)) - D₂*exp(-a′*(x-x₀′)) + V∞ #charged     U_1
 
     (;q, ã, x̃, V̄ₖ) = model
     Vₖ(x) = V̄ₖ * ((1-q)/2*(1 - tanh((x-x̃)/ã)) + q) # coupling energy (Doesn't match the James' paper Eq 28)
