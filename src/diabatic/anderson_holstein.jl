16c4bce42ebf4bf2da7b0f0dbf10c50bef395531
Louhokseson <88427189+Louhokseson@users.noreply.github.com>
2024-04-03 14:18:23 +0100

    comments added

--- src/diabatic/anderson_holstein.jl
+++ src/diabatic/anderson_holstein.jl
@@ -15,13 +15,13 @@ https://link.springer.com/book/10.1007/978-1-4757-5714-9 section 4.2 (Anderson-H
 struct AndersonHolstein{M<:DiabaticModel,B,D,T} <: LargeDiabaticModel
     model::M
     bath::B
-    tmp_derivative::Base.RefValue{D}
+    tmp_derivative::Base.RefValue{D} # temporary array for storing the derivative of the subsystem model 
     fermi_level::T
     nelectrons::Int
 end
 
 function AndersonHolstein(model, bath; fermi_level=0.0)
-    tmp_derivative = Ref(NQCModels.zero_derivative(model, zeros(1,1)))
+    tmp_derivative = Ref(NQCModels.zero_derivative(model, zeros(1,1))) # size of tmp_derivative is 1x1 through Ref() reference can be accessed through []
     fermi_level = austrip(fermi_level)
     # x -> x <= fermi_level :  anonymous function that checks whether its argument x is less than or equal to fermi_level
     # It counts how many states are below or equal to the fermi level
@@ -35,7 +35,7 @@ NQCModels.nelectrons(model::AndersonHolstein) = model.nelectrons
 NQCModels.fermilevel(model::AndersonHolstein) = model.fermi_level
 
 function NQCModels.potential!(model::AndersonHolstein, V::Hermitian, r::AbstractMatrix)
-    Vsystem = NQCModels.potential(model.model, r) # ErpenbeckThoss potential matrix evaluate at r
+    Vsystem = NQCModels.potential(model.model, r) # potential matrix evaluated at r i.e. ErpenbeckThoss is symmetic 2x2 matrix
     V[1,1] = Vsystem[2,2] - Vsystem[1,1] # h(r) = U_1(r) - U_0(r)
     fillbathstates!(V, model.bath) # fill the diagonal elements [2,2]...[N,N] with bath energies (ϵ_k)
     fillbathcoupling!(V, Vsystem[2,1], model.bath) # Vsystem[2,1] Hybridization function component V_k(r)  
@@ -68,9 +68,11 @@ function NQCModels.state_independent_derivative!(model::AndersonHolstein, ∂V::
 end
 
 function get_subsystem_derivative(model::AndersonHolstein, r::AbstractMatrix)
-    if size(r) != size(model.tmp_derivative[])
+    # This function is to calculate the subsystem model's derivative i.e. ErpenbeckThoss model
+    # i.e. model.model: ErpenbeckThoss model
+    if size(r) != size(model.tmp_derivative[]) # model.tmp_derivative[] is a reference to the temporary derivative array
         model.tmp_derivative[] = NQCModels.zero_derivative(model.model, r)
     end
-    NQCModels.derivative!(model.model, model.tmp_derivative[], r)
+    NQCModels.derivative!(model.model, model.tmp_derivative[], r) # calculate the derivative of the subsystem model i.e. ErpenbeckThoss model
     return model.tmp_derivative[]
 end
