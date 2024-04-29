e044fab24b24e35e0692111261b4e7f8170a6adc
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2024-04-29 10:37:43 +0100

    Can't type check FixAtoms constraints so will have to take all constraints in good faith

--- ext/AdvancedASEFunctions.jl
+++ ext/AdvancedASEFunctions.jl
@@ -1,19 +1,13 @@
 module AdvancedASEFunctions
 using NQCModels, PyCall
 
-const ase = PyNULL()
-
-function __init__()
-    copy!(ase, pyimport("ase"))
-end
 
 """
 This module contains methods related to the NQCModels ASE interface that need access to Python types. (e.g. constraint checking)
 """
 
 function NQCModels.mobileatoms(model::NQCModels.AdiabaticModels.AdiabaticASEModel, n::Int)
-	constraints_FixAtoms=isa.(model.atoms.constraints, typeof(ase.constraints.FixAtoms))
-	ase_constraints = [constraint for constraint in model.atoms.constraints[constraints_FixAtoms]]
+	ase_constraints = [constraint for constraint in model.atoms.constraints]
 	if length(ase_constraints) == 0
         return 1:length(model.atoms)
 	else
