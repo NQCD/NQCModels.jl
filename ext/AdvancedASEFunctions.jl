391f0380afe2758966fe47a7e522fbb94cc5860f
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2024-04-29 10:22:57 +0100

    Update constraint logic if no constraints found.

--- ext/AdvancedASEFunctions.jl
+++ ext/AdvancedASEFunctions.jl
@@ -1,14 +1,24 @@
 module AdvancedASEFunctions
 using NQCModels, PyCall
 
+const ase = PyNULL()
+
+function __init__()
+    copy!(ase, pyimport("ase"))
+end
+
 """
-This module contains methods related to the NQCModels ASE interface that need access to Python types. (e.g. constraint checking) 
+This module contains methods related to the NQCModels ASE interface that need access to Python types. (e.g. constraint checking)
 """
 
 function NQCModels.mobileatoms(model::NQCModels.AdiabaticModels.AdiabaticASEModel, n::Int)
-	ase=pyimport("ase")
 	constraints_FixAtoms=isa.(model.atoms.constraints, typeof(ase.constraints.FixAtoms))
-	return symdiff(1:length(model.atoms), [constraint.get_indices() .+ 1 for constraint in model.atoms.constraints[constraints_FixAtoms]]...)
+	ase_constraints = [constraint for constraint in model.atoms.constraints[constraints_FixAtoms]]
+	if length(ase_constraints) == 0
+        return 1:length(model.atoms)
+	else
+        return symdiff(1:length(model.atoms), [constraint.get_indices() .+ 1 for constraint in constraints]...)
+    end
 end
 
 end
