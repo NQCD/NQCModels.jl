5af7567ba00acfb8ed5ec67b29f73f27a7394613
HenrySnowden <snowden.ht@gmail.com>
2025-05-30 13:46:26 +0100

    Added default value to couplings_rescale argument

--- src/diabatic/wide_band_bath_discretisation.jl
+++ src/diabatic/wide_band_bath_discretisation.jl
@@ -8,18 +8,18 @@ function fillbathstates!(out::Hermitian, bath::WideBandBathDiscretisation)
     copy!(diagonal, bath.bathstates)
 end
 
-function fillbathcoupling!(out::Hermitian, coupling::Real, bath::WideBandBathDiscretisation, couplings_rescale::Real)
+function fillbathcoupling!(out::Hermitian, coupling::Real, bath::WideBandBathDiscretisation, couplings_rescale::Real=1.0)
     first_column = @view out.data[2:end, 1]
     setcoupling!(first_column, bath.bathcoupling, coupling, couplings_rescale)
     first_row = @view out.data[1, 2:end]
     copy!(first_row, first_column)
 end
 
-function setcoupling!(out::AbstractVector, bathcoupling::AbstractVector, coupling::Real, couplings_rescale::Real)
+function setcoupling!(out::AbstractVector, bathcoupling::AbstractVector, coupling::Real, couplings_rescale::Real=1.0)
     out .= bathcoupling .* coupling .* couplings_rescale  # bath's states coupling (constant) * Hybridization coupling component V_k(r)
 end
 
-function setcoupling!(out::AbstractVector, bathcoupling::Real, coupling::Real, couplings_rescale::Real)
+function setcoupling!(out::AbstractVector, bathcoupling::Real, coupling::Real, couplings_rescale::Real=1.0)
     fill!(out, bathcoupling * coupling * couplings_rescale)
 end
 
