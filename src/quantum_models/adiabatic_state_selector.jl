617d472df0cabd42c0a93be1975f331ce7578cda
Alexander Spears <alexander.spears@warwick.ac.uk>
2025-06-20 13:08:05 +0100

    🐛 Reformat and attempt to fix NQCDynamics bug

--- src/quantum_models/adiabatic_state_selector.jl
+++ src/quantum_models/adiabatic_state_selector.jl
@@ -1,33 +1,42 @@
 
 struct AdiabaticStateSelector{M} <: NQCModels.ClassicalModels.ClassicalModel
-    model::M
+    quantum_model::M
     state::Int
-    function AdiabaticStateSelector(model, state)
+    function AdiabaticStateSelector(quantum_model, state)
         state < 1 && throw(DomainError(state, "selected state must be greater than 0"))
-        state > NQCModels.nstates(model) && throw(DomainError(state, "selected state must be less than the total number of states of the diabatic model"))
-        return new{typeof(model)}(model, state)
+        state > NQCModels.nstates(quantum_model) && throw(
+            DomainError(
+                state,
+                "selected state must be less than the total number of states of the diabatic model",
+            ),
+        )
+        return new{typeof(quantum_model)}(quantum_model, state)
     end
 end
 
-NQCModels.ndofs(model::AdiabaticStateSelector) = NQCModels.ndofs(model.model)
+NQCModels.ndofs(model::AdiabaticStateSelector) = NQCModels.ndofs(model.quantum_model)
 
 function NQCModels.potential(model::AdiabaticStateSelector, r::AbstractMatrix)
-    V = NQCModels.potential(model.model, r)
+    V = NQCModels.potential(model.quantum_model, r)
     eigenvalues = LinearAlgebra.eigvals(V)
     return eigenvalues[model.state]
 end
 
 function NQCModels.potential!(model::AdiabaticStateSelector, V::Real, r::AbstractMatrix)
-    Vsystem = NQCModels.potential(model.model, r)
+    Vsystem = NQCModels.potential(model.quantum_model, r)
     V .= LinearAlgebra.eigvals(Vsystem)[model.state]
 end
 
-function NQCModels.derivative!(model::AdiabaticStateSelector, output::AbstractMatrix, r::AbstractMatrix)
-    V = NQCModels.potential(model.model, r)
+function NQCModels.derivative!(
+    model::AdiabaticStateSelector,
+    output::AbstractMatrix,
+    r::AbstractMatrix,
+)
+    V = NQCModels.potential(model.quantum_model, r)
     U = LinearAlgebra.eigvecs(V)
-    D = [NQCModels.derivative(model.model, r)]
+    D = NQCModels.derivative(model.quantum_model, r)
     for I in eachindex(output, D)
-        output[I] = (U' * D[I] * U)[model.state, model.state]
+        output[I] = (U'*D[I]*U)[model.state, model.state]
     end
     return output
 end
