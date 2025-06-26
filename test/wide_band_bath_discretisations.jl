497c08995327043fa496a2454578235313570312
Matt Larkin <61248744+mlarkin863@users.noreply.github.com>
2025-06-26 18:39:07 +0100

    slim down WTR implementation and writing tests

--- test/wide_band_bath_discretisations.jl
+++ test/wide_band_bath_discretisations.jl
@@ -16,6 +16,18 @@ using LinearAlgebra: Hermitian, diagind
     @test all(isapprox.(out[2:end,1], 1.0 / sqrt(50/20)))
 end
 
+@testset "WindowedTrapezoidalRule" begin
+    bath = NQCModels.WindowedTrapezoidalRule(50, -10, 10)
+    n = NQCModels.nstates(bath)
+    out = Hermitian(zeros(n+1, n+1))
+
+    NQCModels.DiabaticModels.fillbathstates!(out, bath)
+    @test all(out[diagind(out)[2:end]] .== bath.bathstates)
+    NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath)
+    @test all(isapprox.(out[1,2:end], 1.0 / sqrt(50/20)))
+    @test all(isapprox.(out[2:end,1], 1.0 / sqrt(50/20)))
+end
+
 @testset "ShenviGaussLegendre" begin
     bath = NQCModels.ShenviGaussLegendre(20, -10, 10.0)
 
