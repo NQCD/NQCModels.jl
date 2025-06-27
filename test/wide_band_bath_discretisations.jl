7cf0f549e9602976c014ae28135f0d383b834ae3
Matt Larkin <61248744+mlarkin863@users.noreply.github.com>
2025-06-27 08:05:32 +0100

    added some tests to WTR

--- test/wide_band_bath_discretisations.jl
+++ test/wide_band_bath_discretisations.jl
@@ -17,15 +17,22 @@ using LinearAlgebra: Hermitian, diagind
 end
 
 @testset "WindowedTrapezoidalRule" begin
-    bath = NQCModels.WindowedTrapezoidalRule(50, -10, 10)
+    DR = 0.50 # densityratio (0.50 is the default value)
+    bath = NQCModels.WindowedTrapezoidalRule(60, -10, 10, -1, 1, densityratio=DR)
     n = NQCModels.nstates(bath)
     out = Hermitian(zeros(n+1, n+1))
 
     NQCModels.DiabaticModels.fillbathstates!(out, bath)
     @test all(out[diagind(out)[2:end]] .== bath.bathstates)
+    
     NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath)
-    @test all(isapprox.(out[1,2:end], 1.0 / sqrt(50/20)))
-    @test all(isapprox.(out[2:end,1], 1.0 / sqrt(50/20)))
+    N_window = Int(60 * DR)
+    ΔE_window = 1 - -1
+    N_sparse = Int(0.5 * N_window)
+    ΔE_sparse = 10 - 1
+
+    @test all(isapprox.(out[1,(2+N_sparse):(N_sparse + N_window + 1 )], 1.0 / sqrt((N_window-1)/ΔE_window)))
+    @test all(isapprox.(out[(2+N_sparse):(N_sparse + N_window + 1 ),1], 1.0 / sqrt((N_window-1)/ΔE_window)))
 end
 
 @testset "ShenviGaussLegendre" begin
