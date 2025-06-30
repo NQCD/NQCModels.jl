e33e22ccb7d88a207d13d34135c89d2ff29da620
Nhertl <Nils.Hertl@warwick.ac.uk>
2025-06-30 15:51:08 +0100

    Change variable name of 'coupling_rescale'
    Changed variable name of 'coupling_rescale' to 'couplings_rescale' to resolve an issue with the main branch.

--- src/diabatic/wide_band_bath_discretisation.jl
+++ src/diabatic/wide_band_bath_discretisation.jl
@@ -8,19 +8,19 @@ function fillbathstates!(out::Hermitian, bath::WideBandBathDiscretisation)
     copy!(diagonal, bath.bathstates)
 end
 
-function fillbathcoupling!(out::Hermitian, coupling::Real, bath::WideBandBathDiscretisation, coupling_rescale::Real=1.0)
+function fillbathcoupling!(out::Hermitian, coupling::Real, bath::WideBandBathDiscretisation, couplings_rescale::Real=1.0)
     first_column = @view out.data[2:end, 1]
-    setcoupling!(first_column, bath.bathcoupling, coupling, coupling_rescale)
+    setcoupling!(first_column, bath.bathcoupling, coupling, couplings_rescale)
     first_row = @view out.data[1, 2:end]
     copy!(first_row, first_column)
 end
 
-function setcoupling!(out::AbstractVector, bathcoupling::AbstractVector, coupling::Real, coupling_rescale::Real=1.0)
-    out .= bathcoupling .* coupling .* coupling_rescale
+function setcoupling!(out::AbstractVector, bathcoupling::AbstractVector, coupling::Real, couplings_rescale::Real=1.0)
+    out .= bathcoupling .* coupling .* couplings_rescale
 end
 
-function setcoupling!(out::AbstractVector, bathcoupling::Real, coupling::Real, coupling_rescale::Real=1.0)
-    fill!(out, bathcoupling * coupling .* coupling_rescale)
+function setcoupling!(out::AbstractVector, bathcoupling::Real, coupling::Real, couplings_rescale::Real=1.0)
+    fill!(out, bathcoupling * coupling .* couplings_rescale)
 end
 
 """
