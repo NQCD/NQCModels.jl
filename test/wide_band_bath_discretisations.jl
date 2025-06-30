d94943e0bd237fbc3dd2041b00966f6bae6effbe
Nhertl <Nils.Hertl@warwick.ac.uk>
2025-06-30 15:53:12 +0100

    Changed variable name 'coupling_rescale'
    Changed name of variable 'coupling_rescale' to 'couplings_rescale' in order to resolve an existing conflict with main branch.

--- test/wide_band_bath_discretisations.jl
+++ test/wide_band_bath_discretisations.jl
@@ -8,11 +8,11 @@ using LinearAlgebra: Hermitian, diagind
     bath = NQCModels.TrapezoidalRule(50, -10, 10)
     n = NQCModels.nstates(bath)
     out = Hermitian(zeros(n+1, n+1))
-    coupling_rescale = 1.0
+    couplings_rescale = 1.0
 
     NQCModels.DiabaticModels.fillbathstates!(out, bath)
     @test all(out[diagind(out)[2:end]] .== bath.bathstates)
-    NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath, coupling_rescale)
+    NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath, couplings_rescale)
     @test all(isapprox.(out[1,2:end], 1.0 / sqrt(50/20)))
     @test all(isapprox.(out[2:end,1], 1.0 / sqrt(50/20)))
 end
@@ -22,10 +22,10 @@ end
 
     n = NQCModels.nstates(bath)
     out = Hermitian(zeros(n+1, n+1))
-    coupling_rescale = 1.0
+    couplings_rescale = 1.0
 
     NQCModels.DiabaticModels.fillbathstates!(out, bath)
-    NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath, coupling_rescale)
+    NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath, couplings_rescale)
 end
 
 @testset "ReferenceGaussLegendre" begin
@@ -33,10 +33,10 @@ end
 
     n = NQCModels.nstates(bath)
     out = Hermitian(zeros(n+1, n+1))
-    coupling_rescale = 1.0
+    couplings_rescale = 1.0
 
     NQCModels.DiabaticModels.fillbathstates!(out, bath)
-    NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath, coupling_rescale)
+    NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath, couplings_rescale)
 end
 
 @testset "FullGaussLegendre" begin
@@ -44,8 +44,8 @@ end
 
     n = NQCModels.nstates(bath)
     out = Hermitian(zeros(n+1, n+1))
-    coupling_rescale = 1.0
+    couplings_rescale = 1.0
 
     NQCModels.DiabaticModels.fillbathstates!(out, bath)
-    NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath, coupling_rescale)
+    NQCModels.DiabaticModels.fillbathcoupling!(out, 1.0, bath, couplings_rescale)
 end
